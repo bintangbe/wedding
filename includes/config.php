4172a021486ea58e40510ccf48a7ce41ddb9f0a0
bintangbe <bintangazissatriowibowo@gmail.com>
2024-12-24 05:14:04 +0700

    dockear fiaxxx
    - tampilan website blank

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,15 @@
 <?php
 
-$koneksidb = new mysqli('localhost','root','','wedding');
+$host = 'db'; // Nama service database di docker-compose.yml
+$username = 'root'; // Harus sesuai dengan MYSQL_USER
+$password = '123456'; // Harus sesuai dengan MYSQL_PASSWORD
+$database = 'wedding_db'; // Harus sesuai dengan MYSQL_DATABASE
 
+$mysqli = new mysqli($host, $username, $password, $database);
 
-if ( $koneksidb) {
-  echo "KONEK !";
+// Periksa apakah ada kesalahan
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
 }
 
 ?>
--- includes/header.php
+++ includes/header.php
@@ -3,28 +3,25 @@
     <div class="container">
       <div class="row">
         <div class="col-sm-3 col-md-4">
-        
-          <div class="logo"><a href="index.php"><img src="admin/img/newicon.jpeg" width="220px" height="80px" alt="image"/></a> </div><br><br><br><br> 
+
+          <div class="logo"><a href="index.php"><img src="admin/img/newicon.jpeg" width="220px" height="80px" alt="image" /></a> </div><br><br><br><br>
           <p style="color:#ffd700"> Wedding Decoration </p>
         </div>
         <div class="col-sm-8 col-md-8">
-          
-			<?php   if(strlen($_SESSION['ulogin'])==0)
-			{	
-			?>
-			<div class="login_btn"> 
-				<a href="#loginform" class="btn btn-warning btn-xs uppercase" data-toggle="modal" data-dismiss="modal">Login / Register</a> 
-			</div>
-			<?php }
-			else{ 
-			
-			} ?>
-          </div>
+
+          <?php if (strlen($_SESSION['ulogin']) == 0) {
+          ?>
+            <div class="login_btn">
+              <a href="#loginform" class="btn btn-warning btn-xs uppercase" data-toggle="modal" data-dismiss="modal">Login / Register</a>
+            </div>
+          <?php } else {
+          } ?>
         </div>
       </div>
     </div>
   </div>
-  
+  </div>
+
   <!-- Navigation -->
   <nav id="navigation_bar" class="navbar navbar-default">
     <div class="container">
@@ -34,39 +31,39 @@
       <div class="header_wrap">
         <div class="user_login">
           <ul>
-            <li class="dropdown"> <a href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user-circle" aria-hidden="true"></i> 
-<?php 
-$username=$_SESSION['ulogin'];
-$sql ="SELECT nama_user FROM member WHERE username='$username'";
-$query = mysqli_query($koneksidb,$sql);
-if(mysqli_num_rows($query)>0)
-{
-while($results = mysqli_fetch_array($query))
-	{
-	 echo htmlentities($results['nama_user']); }}?>
-	 <i class="fa fa-angle-down" aria-hidden="true"></i></a>
-          <ul class="dropdown-menu">
-           <?php if($_SESSION['ulogin']){?>
-            <li><a href="profile.php">Profile Settings</a></li>
-              <li><a href="update-password.php">Update Password</a></li>
-            <li><a href="riwayatsewa.php">Riwayat Sewa</a></li>
-            <li><a href="logout.php">Sign Out</a></li>
-            <?php }?>
-          </ul>
+            <li class="dropdown"> <a href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user-circle" aria-hidden="true"></i>
+                <?php
+                $username = $_SESSION['ulogin'];
+                $sql = "SELECT nama_user FROM member WHERE username='$username'";
+                $query = mysqli_query($koneksidb, $sql);
+                if (mysqli_num_rows($query) > 0) {
+                  while ($results = mysqli_fetch_array($query)) {
+                    echo htmlentities($results['nama_user']);
+                  }
+                } ?>
+                <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+              <ul class="dropdown-menu">
+                <?php if ($_SESSION['ulogin']) { ?>
+                  <li><a href="profile.php">Profile Settings</a></li>
+                  <li><a href="update-password.php">Update Password</a></li>
+                  <li><a href="riwayatsewa.php">Riwayat Sewa</a></li>
+                  <li><a href="logout.php">Sign Out</a></li>
+                <?php } ?>
+              </ul>
             </li>
           </ul>
         </div>
       </div>
-	  	  
+
       <div class="collapse navbar-collapse" id="navigation">
         <ul class="nav navbar-nav">
-		  <li><a href="index.php">Home</a></li>
+          <li><a href="index.php">Home</a></li>
           <li><a href="paket.php">Daftar Paket</a></li>
           <li><a href="page.php?type=aboutus">Tentang Kami</a></li>
         </ul>
       </div>
     </div>
   </nav>
-  <!-- Navigation end --> 
-  
+  <!-- Navigation end -->
+
 </header>
